f9a3530626c539fbfcc1a99547d65ab70ed92d54
vibhasvats <vkvatsdss@gmail.com>
2026-04-22 13:54:49 -0400

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// Replace with your GitHub Pages URL: https://<username>.github.io/<repo>/
 export default defineConfig({
   site: 'https://rainbowofimagination.github.io',
-  // base: '',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
